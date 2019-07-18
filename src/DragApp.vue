5a7e47531d641dadeee72a0898302acbccd31b54
ph2 <hydrogenpan2019@gmail.com>
2019-07-18 19:55:42 +0800

    add draggable-vue-directive-problem example

---
+++ src/DragApp.vue
@@ -0,0 +1,201 @@
+<template>
+  <div class="main">
+
+    <div class="control-group">
+      <div class="control-btn" @click="addBox">
+        Add Box
+      </div>
+      <div class="control-btn" @click="clearAll">
+        Clear All
+      </div>
+    </div>
+
+    <div>
+      <img src="./assets/logo.png" alt="Vue Logo">
+    </div>
+
+    <div v-for="boxNumber in boxes" :key="boxNumber" v-draggable class="draggable-container">
+      Simple Draggable
+    </div>
+
+    <div v-draggable="draggableWithHandler" class="draggable-with-handler">
+      Use Handler To Drag Me
+      <div ref="handler" class="handler">
+        I am Handler
+      </div>
+    </div>
+
+    <div ref="bounder" class="container-for-draggable">
+      <div v-draggable="draggableWithBoundries" class="draggable-container">
+        drag me within my boundries
+      </div>
+    </div>
+
+    <div v-draggable="draggableWithResetPosition" class="draggable-with-reset" @dblclick="onReset">
+      drag me and double click me to reset my position
+    </div>
+
+    <div class="autor">
+      <span>Autor</span>
+      Israel Zablianov
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+  import {Draggable} from 'draggable-vue-directive'
+
+  function getDragCallback(name) {
+    function dragCallback(positionDiff, absolutePosition, event = {}) {
+      // eslint-disable-next-line no-console
+      console.log(name, absolutePosition, positionDiff, {clientX: event.clientX, clientY: event.clientY})
+    }
+
+    return dragCallback
+  }
+
+  export default {
+    name: 'Demo',
+
+    directives: {
+      Draggable,
+    },
+
+    data() {
+      return {
+        boxes: 0,
+
+        draggableWithHandler: {
+          handle: undefined,
+          onPositionChange: getDragCallback('onPositionChange'),
+          onDragStart: getDragCallback('onDragStart'),
+          onDragEnd: getDragCallback('onDragEnd'),
+        },
+
+        draggableWithBoundries: {},
+        draggableWithResetPosition: {resetInitialPos: false},
+      }
+    },
+
+    mounted() {
+      this.draggableWithBoundries.boundingElement = this.$refs.bounder
+      this.draggableWithBoundries.boundingRectMargin = {
+        top: 2,
+        bottom: 2,
+        left: 2,
+        right: 2,
+      }
+
+      this.draggableWithHandler.handle = this.$refs.handler
+    },
+
+    methods: {
+      addBox() {
+        this.boxes++
+      },
+
+      clearAll() {
+        this.boxes = 0
+      },
+
+      onReset() {
+        this.draggableWithResetPosition.resetInitialPos = true
+        setTimeout(() => {
+          this.draggableWithResetPosition.resetInitialPos = false
+        }, 0)
+      },
+    },
+  }
+</script>
+
+<style lang="scss">
+  .main {
+    display: flex;
+    justify-content: space-around;
+    height: 100%;
+    width: 100%;
+  }
+
+  .control-group {
+    display: flex;
+    flex-direction: column;
+    margin-top: 25px;
+  }
+
+  .control-btn {
+    border-radius: 3px;
+    background-color: #42b883;
+    color: #35495e;
+    cursor: pointer;
+    padding: 15px;
+    margin: 15px;
+  }
+
+  @mixin vue-box() {
+    background: white;
+    border: 3px solid #42b883;
+    border-radius: 3px;
+    text-align: center;
+    font-size: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .draggable-container {
+    width: 150px;
+    height: 150px;
+    @include vue-box();
+  }
+
+  @mixin container-for-draggable() {
+    @include vue-box();
+    position: absolute;
+    align-self: flex-end;
+    width: 300px;
+    height: 200px;
+    bottom: 20px;
+  }
+
+  .container-for-draggable {
+    @include container-for-draggable();
+  }
+
+  .draggable-with-handler {
+    @include container-for-draggable();
+    left: 20px;
+  }
+
+  .handler {
+    padding: 5px;
+    position: absolute;
+    top: 0;
+    left: 70px;
+    margin-top: -45px;
+    @include vue-box();
+  }
+
+  .draggable-with-reset {
+    @include container-for-draggable();
+    right: 20px;
+  }
+
+  .autor {
+    align-self: flex-start;
+    margin-top: 25px;
+    margin-bottom: 20px;
+    padding: 10px;
+    border-radius: 3px;
+    color: #42b883;
+    background-color: #35495e;
+    font-size: 25px;
+    font-weight: bolder;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
